4c6aed04212043587ca5d8d8a0d3e09becb7d043
MarinaManushina <122769289+MarinaManushina@users.noreply.github.com>
2023-06-03 08:53:37 +0300

    Update Main.java

--- lb7/src/Main.java
+++ lb7/src/Main.java
@@ -1,41 +1,57 @@
+import java.util.Scanner;
 public class Main {
-    public class CalculateValues {
-        private double a, x, b;
-        CalculateValues(double a, double x, double b){
-            this.a = a;
-            this.b = b;
-            this.x = x;
+    public void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите a: ");
+        double a = scan.nextDouble();
+        System.out.print("Введите b: ");
+        double b = scan.nextDouble();
+        System.out.print("Введите х: ");
+        double x = scan.nextDouble();
+
+        Match zn1 = new Match(a, b, x);
+        zn1.func1(x);
+        zn1.func2(a, b);
+        zn1.func3(a, b, x);
+    }
+}
+class Match {
+    double a, b, x;
+    Match(double a, double b, double x){
+        this.a = a;
+        this.b = b;
+        this.x = x;
+    }
+    public void func1(double x) {
+        double rez = 3*x+5;
+        System.out.println(rez);
+    }
+    public void  func2(double a, double b){
+        if(a-b != 0) {
+            double rez = (a+b)/(a-b);
+            System.out.println(rez);
+        } else {
+            System.out.println("Ошибка, в знаменателе 0!");
         }
-        CalculateValues(double[] arr){
-            this.a = arr[0];
-            this.b = arr[1];
-            this.x = arr[2];
-        }
-        public double calculate1() {
-            return 3 * this.x + 5;
-        }
-        double factorial(double n) {
-            if (n == 0) {
-                return 1;
+    }
+    public void func3(double a, double b, double x){
+        if (b != 0){
+            double rez = a*x/b;
+            double rez1 = rez - Math.floor(rez);
+            if (rez1 == 0.0) {
+                System.out.println(getFactorial(rez));
+            } else {
+                System.out.println("У нецелого числа нет факториала!");
             }
-            return n * factorial(n - 1);
+        } else {
+            System.out.println("Ошибка, в знаменателе 0!");
         }
-        public double calculate2() {
-            if(a-b!=0){
-                return (a + b) / (a - b);}
-            else{
-                return -1;}
+    }
+    public int getFactorial(double y) {
+        int result = 1;
+        for (int i = 1; i <= y; i++) {
+            result = result * i;
         }
-        public double calculate3() {
-            if(b!=0) {
-                double temp = (a * x) / b;
-                if(temp%1==0){
-                    return factorial(temp);}
-                else{
-                    return -1;}
-            }
-            else{
-                return -1;}
-        }
+        return result;
     }
 }
